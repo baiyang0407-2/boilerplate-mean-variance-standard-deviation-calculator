db4f068e4aa3e8aae7edea697f53e36ca8283bb0
baiyang0407-2 <baiyang0407@gmail.com>
2026-01-06 23:40:19 +0000

    complete

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,28 @@
 import numpy as np
 
 def calculate(list):
+    if len(list)<9:
+        raise ValueError("List must contain nine numbers.")
+    matrix=np.array(list)
+    matrix.shape=(3,3)
+
+    
+    sol=dict()
+    sol['mean'] = [matrix.mean(0).tolist(),matrix.mean(1).tolist(),np.array(list).mean().tolist()]
+    sol['variance'] = [matrix.var(0).tolist(),matrix.var(1).tolist(),np.array(list).var().tolist()]
+    sol['standard deviation'] = [matrix.std(0).tolist(),matrix.std(1).tolist(),np.array(list).std().tolist()]
+    sol['max'] =[ matrix.max(0).tolist(),matrix.max(1).tolist(),np.array(list).max().tolist()]
+    sol['min'] = [matrix.min(0).tolist(),matrix.min(1).tolist(), np.array(list).min().tolist()]
+    sol['sum'] = [matrix.sum(0).tolist(),matrix.sum(1).tolist(),np.array(list).sum().tolist()]
+
+    return sol
 
 
 
 
-    return calculations
+    
+
+
+
+
+    
